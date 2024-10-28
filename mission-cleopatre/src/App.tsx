aa342211f925622ca408ded56a6fe6493c55514f
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-10-28 15:28:30 +0100

    completed the objects section in the App.tsx file

--- mission-cleopatre/src/App.tsx
+++ mission-cleopatre/src/App.tsx
@@ -7,7 +7,35 @@ function App() {
 			charName: "Idefix",
 			charOrigin: "gaulois",
 			imgSrc: "/Images/idefix.jpg",
+			shortenedLegend: "",
+			fullLegend: "",
+		},
+
+		{
+			charName: "César",
+			charOrigin: "romain",
+			imgSrc: "/Images/cesar.jpg",
 			description: "",
+			shortenedLegend: "",
+			fullLegend: "",
+		},
+
+		{
+			charName: "Cléopâtre",
+			charOrigin: "égyptien",
+			imgSrc: "/Images/cleopatre.jpg",
+			description: "",
+			shortenedLegend: "",
+			fullLegend: "",
+		},
+
+		{
+			charName: "Numérobis",
+			charOrigin: "égyptien",
+			imgSrc: "/Images/numerobis.jpg",
+			description: "",
+			shortenedLegend: "",
+			fullLegend: "",
 		},
 
 		{
@@ -15,6 +43,26 @@ function App() {
 			charOrigin: "gaulois",
 			imgSrc: "/Images/asterix.jpg",
 			description: "",
+			shortenedLegend: "",
+			fullLegend: "",
+		},
+
+		{
+			charName: "Caius Antivirus",
+			charOrigin: "romain",
+			imgSrc: "/Images/caius-antivirus.jpg",
+			description: "",
+			shortenedLegend: "",
+			fullLegend: "",
+		},
+
+		{
+			charName: "Cartapus",
+			charOrigin: "romain",
+			imgSrc: "/Images/cartapus.jpg",
+			description: "",
+			shortenedLegend: "",
+			fullLegend: "",
 		},
 
 		{
@@ -22,48 +70,26 @@ function App() {
 			charOrigin: "gaulois",
 			imgSrc: "/Images/obelix.jpg",
 			description: "",
+			shortenedLegend: "",
+			fullLegend: "",
 		},
-		{
-			charName: "Panoramix",
-			charOrigin: "gaulois",
-			imgSrc: "/Images/panoramix.jpg",
-			description: "",
-		},
-		{
-			charName: "César",
-			charOrigin: "romain",
-			imgSrc: "/Images/cesar.jpg",
-			description: "",
-		},
-		{
-			charName: "Caius Antivirus",
-			charOrigin: "romain",
-			imgSrc: "/Images/caius-antivirus.jpg",
-			description: "",
-		},
-		{
-			charName: "Cartapus",
-			charOrigin: "romain",
-			imgSrc: "/Images/cartapus.jpg",
-			description: "",
-		},
-		{
-			charName: "Cléopâtre",
-			charOrigin: "égyptien",
-			imgSrc: "/Images/cleopatre.jpg",
-			description: "",
-		},
-		{
-			charName: "Numérobis",
-			charOrigin: "égyptien",
-			imgSrc: "/Images/numerobis.jpg",
-			description: "",
-		},
+
 		{
 			charName: "Amonbeaufils",
 			charOrigin: "égyptien",
 			imgSrc: "/Images/amonbeaufils.jpg",
 			description: "",
+			shortenedLegend: "",
+			fullLegend: "",
+		},
+
+		{
+			charName: "Panoramix",
+			charOrigin: "gaulois",
+			imgSrc: "/Images/panoramix.jpg",
+			description: "",
+			shortenedLegend: "",
+			fullLegend: "",
 		},
 	];
 
