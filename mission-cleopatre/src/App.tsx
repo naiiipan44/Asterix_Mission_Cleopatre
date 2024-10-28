f906ded015d02ae0ce1f4c1cb335a96b077757d2
CyrusWeir <cyrusweir@gmail.com>
2024-10-28 12:35:20 +0100

    began trying to code the mapping of the cards. Lots of errors for the moment

--- mission-cleopatre/src/App.tsx
+++ mission-cleopatre/src/App.tsx
@@ -1,12 +1,23 @@
 // import { useState } from 'react'
 import './App.css'
+import Card from './Components/Card'
+
+const TestcharacterList = [
+  {charName: "Cyrus",
+    imgSrc: "./Images/logo3-normal.png",
+    charOrigin: "Australia",
+    description: "just the best"
+
+
+  }
+]
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      //your code here
+      <Card characterList={TestcharacterList[0]}/>
     </>
   )
 }
--- mission-cleopatre/src/Components/Card.tsx
+++ mission-cleopatre/src/Components/Card.tsx
@@ -1,8 +1,25 @@
-function card () {
-  return(
-    <figure>
-<img src={imgSrc} alt=`image de ${charName}`>
-    </figure>
+import '..Style/Card.css'
+interface charProps {
+  characterList: TestcharacterList[];
+  
+  // charName: string,
+  // imgSrc: string,
+  // charOrigin: string,
+  // description: string,
+  }
+
+function Card ({characterList} : charProps) {characterList.map((character) => {
+      return(
+<figure>
+<img src={character.imgSrc} alt=`image de ${character.charName}` />
+<figcaption>
+<h2>{character.charName}</h2>
+<h3>{character.charOrigin}</h3>
+<p>{character.description}</p>
+</figcaption>
+</figure>
   )
 }
-export default card
+
+
+export default Card
