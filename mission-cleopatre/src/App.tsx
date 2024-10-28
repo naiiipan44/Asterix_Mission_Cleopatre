38b960f021079fd235365d52ea22bdea0cf3ec49
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-10-28 12:29:40 +0100

    Added the object of the file

--- mission-cleopatre/src/App.tsx
+++ mission-cleopatre/src/App.tsx
@@ -1,14 +1,73 @@
 // import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+	const char = [
+		{
+			charName: "Idefix",
+			charOrigin: "gaulois",
+			imgSrc: "/Images/idefix.jpg",
+			description: "",
+		},
 
-  return (
-    <>
-      //your code here
-    </>
-  )
+		{
+			charName: "Astérix",
+			charOrigin: "gaulois",
+			imgSrc: "/Images/asterix.jpg",
+			description: "",
+		},
+
+		{
+			charName: "Obélix",
+			charOrigin: "gaulois",
+			imgSrc: "/Images/obelix.jpg",
+			description: "",
+		},
+		{
+			charName: "Panoramix",
+			charOrigin: "gaulois",
+			imgSrc: "/Images/panoramix.jpg",
+			description: "",
+		},
+		{
+			charName: "César",
+			charOrigin: "romain",
+			imgSrc: "/Images/cesar.jpg",
+			description: "",
+		},
+		{
+			charName: "Caius Antivirus",
+			charOrigin: "romain",
+			imgSrc: "/Images/caius-antivirus.jpg",
+			description: "",
+		},
+		{
+			charName: "Cartapus",
+			charOrigin: "romain",
+			imgSrc: "/Images/cartapus.jpg",
+			description: "",
+		},
+		{
+			charName: "Cléopâtre",
+			charOrigin: "égyptien",
+			imgSrc: "/Images/cleopatre.jpg",
+			description: "",
+		},
+		{
+			charName: "Numérobis",
+			charOrigin: "égyptien",
+			imgSrc: "/Images/numerobis.jpg",
+			description: "",
+		},
+		{
+			charName: "Amonbeaufils",
+			charOrigin: "égyptien",
+			imgSrc: "/Images/amonbeaufils.jpg",
+			description: "",
+		},
+	];
+
+	return <></>;
 }
 
-export default App
+export default App;
