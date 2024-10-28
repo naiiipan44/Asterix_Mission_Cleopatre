bc9f83cc149c7e5635db2582c12564b9cad0f32c
Clément Praud <clement.praud6@gmail.com>
2024-10-28 16:54:29 +0100

    Add filter on card charName

--- mission-cleopatre/src/App.tsx
+++ mission-cleopatre/src/App.tsx
@@ -1,43 +1,16 @@
 // import { useState } from 'react'
 
-<<<<<<< HEAD
-// =======
-=======
->>>>>>> a3b06071139d212545abd4db5ed2689e463e9524
 import "./App.css";
-// import Card from "./Components/Card"
+import Card from "./Components/Card"
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
-function App() {
-	return (
-		<>
-			<Header />
-			<section>{/* <Card /> */}</section>
-			<section>
-				<Footer />
-			</section>
-		</>
-	);
-}
 
 const char = [
 	{
 		charName: "Idefix",
 		charOrigin: "gaulois",
-<<<<<<< HEAD
 		imgSrc: "./src/Images/idefix.jpg",
-=======
-		imgSrc: "/Images/idefix.jpg",
-		shortenedLegend: "",
-		fullLegend: "",
-	},
-
-	{
-		charName: "César",
-		charOrigin: "romain",
-		imgSrc: "/Images/cesar.jpg",
->>>>>>> a3b06071139d212545abd4db5ed2689e463e9524
 		description: "",
 		shortenedLegend: "",
 		fullLegend: "",
@@ -46,7 +19,7 @@ const char = [
 	{
 		charName: "Cléopâtre",
 		charOrigin: "égyptien",
-		imgSrc: "/Images/cleopatre.jpg",
+		imgSrc: "./src/Images/cleopatre.jpg",
 		description: "",
 		shortenedLegend: "",
 		fullLegend: "",
@@ -55,7 +28,7 @@ const char = [
 	{
 		charName: "Numérobis",
 		charOrigin: "égyptien",
-		imgSrc: "/Images/numerobis.jpg",
+		imgSrc: "./src/Images/numerobis.jpg",
 		description: "",
 		shortenedLegend: "",
 		fullLegend: "",
@@ -73,7 +46,7 @@ const char = [
 	{
 		charName: "Caius Antivirus",
 		charOrigin: "romain",
-		imgSrc: "/Images/caius-antivirus.jpg",
+		imgSrc: "./src/Images/caius-antivirus.jpg",
 		description: "",
 		shortenedLegend: "",
 		fullLegend: "",
@@ -82,7 +55,7 @@ const char = [
 	{
 		charName: "Cartapus",
 		charOrigin: "romain",
-		imgSrc: "/Images/cartapus.jpg",
+		imgSrc: "./src/Images/cartapus.jpg",
 		description: "",
 		shortenedLegend: "",
 		fullLegend: "",
@@ -96,46 +69,6 @@ const char = [
 		shortenedLegend: "",
 		fullLegend: "",
 	},
-<<<<<<< HEAD
-	{
-		charName: "Panoramix",
-		charOrigin: "gaulois",
-		imgSrc: "./src/Images/panoramix.jpg",
-		description: "",
-	},
-	{
-		charName: "César",
-		charOrigin: "romain",
-		imgSrc: "./src/Images/cesar.jpg",
-		description: "",
-	},
-	{
-		charName: "Caius Antivirus",
-		charOrigin: "romain",
-		imgSrc: "./src/Images/caius-antivirus.jpg",
-		description: "",
-	},
-	{
-		charName: "Cartapus",
-		charOrigin: "romain",
-		imgSrc: "./src/Images/cartapus.jpg",
-		description: "",
-	},
-	{
-		charName: "Cléopâtre",
-		charOrigin: "égyptien",
-		imgSrc: "./src/Images/cleopatre.jpg",
-		description: "",
-	},
-	{
-		charName: "Numérobis",
-		charOrigin: "égyptien",
-		imgSrc: "./src/Images/numerobis.jpg",
-		description: "",
-	},
-=======
-
->>>>>>> a3b06071139d212545abd4db5ed2689e463e9524
 	{
 		charName: "Amonbeaufils",
 		charOrigin: "égyptien",
@@ -148,17 +81,15 @@ const char = [
 	{
 		charName: "Panoramix",
 		charOrigin: "gaulois",
-		imgSrc: "/Images/panoramix.jpg",
+		imgSrc: "./src/Images/panoramix.jpg",
 		description: "",
 		shortenedLegend: "",
 		fullLegend: "",
 	},
 ];
-<<<<<<< HEAD
-// >>>>>>> 62251a923bb542b29de947b787d3f375c944b7e3
+
 
 function App() {
-	// const [count, setCount] = useState(0)
 
 	return (
 		<>
@@ -171,9 +102,7 @@ function App() {
 			</section>
 		</>
 	);
-	// >>>>>>> 62251a923bb542b29de947b787d3f375c944b7e3
+
 }
-=======
->>>>>>> a3b06071139d212545abd4db5ed2689e463e9524
 
 export default App;
--- mission-cleopatre/src/Components/Card.tsx
+++ mission-cleopatre/src/Components/Card.tsx
@@ -1,29 +1,41 @@
-<<<<<<< HEAD
-
 import '../Style/Card.css'
+import { useState } from 'react'
+
 interface charProps {
   characterList:{
     charName: string,
     imgSrc: string,
     charOrigin: string,
-    description: string,
+    shortenedLegend: string,
+		fullLegend: string
   }}
 
 function Card ({characterList} : charProps) {
+
+  const [input, setInput] = useState<string>("");
+  const handleChange = (event) => {
+    console.log(input);
+    setInput(event.target.value);
+  }
+
  return(
-  <div className='charDiv'>
-    {characterList.map((character) => (
-			<figure key={character.charName}>
-				<img src={character.imgSrc} alt={character.charName} />
-				<figcaption>
-					<h2>{character.charName}</h2>
-					<h3>{character.charOrigin}</h3>
-					<p>{character.description}</p>
-				</figcaption>
-			</figure>
+  <>
+    <input onChange={handleChange} type="text" placeholder='Filtre les personnages !'></input>
+    <div className='charDiv'>
+      {characterList.filter((yourmom) => (
+        yourmom.charName.includes(input)
+      )).map((character) => (
+          <figure key={character.charName}>
+            <img src={character.imgSrc} alt={character.charName} />
+            <figcaption>
+              <h2>{character.charName}</h2>
+              <h3>{character.charOrigin}</h3>
+              {/* <p>{character.description}</p> */}
+            </figcaption>
+          </figure>
 		))}
   </div>
-
+  </>
 
   )
 }
